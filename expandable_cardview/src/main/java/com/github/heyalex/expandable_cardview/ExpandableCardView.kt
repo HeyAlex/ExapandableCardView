a7df949a7c84116a1a4c5145dc8df0519c98bedf
a.fialko <a.fialko@corp.mail.ru>
2019-02-24 22:59:04 +0300

    inflate into viewstub

--- expandable_cardview/src/main/java/com/github/heyalex/expandable_cardview/ExpandableCardView.kt
+++ expandable_cardview/src/main/java/com/github/heyalex/expandable_cardview/ExpandableCardView.kt
@@ -4,8 +4,9 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
-import androidx.annotation.IntegerRes
+import androidx.annotation.LayoutRes
 import androidx.cardview.widget.CardView
+import kotlinx.android.synthetic.main.expandable_cardview.view.*
 
 class ExpandableCardView @JvmOverloads constructor(
     context: Context,
@@ -25,9 +26,20 @@ class ExpandableCardView @JvmOverloads constructor(
     init {
         LayoutInflater.from(context).inflate(R.layout.expandable_cardview, this)
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ExpandableCardView)
-        headerViewRes = typedArray.getResourceId(R.styleable.ExpandableCardView_header_view, View.NO_ID)
-        contentViewRes = typedArray.getResourceId(R.styleable.ExpandableCardView_content_view, View.NO_ID)
+        headerViewRes =
+            typedArray.getResourceId(R.styleable.ExpandableCardView_header_view, View.NO_ID)
+        contentViewRes =
+            typedArray.getResourceId(R.styleable.ExpandableCardView_content_view, View.NO_ID)
         typedArray.recycle()
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        card_header.layoutResource = headerViewRes
+        headerView = card_header.inflate()
+
+        card_content.layoutResource = contentViewRes
+        contentView = card_content.inflate()
     }
 
 }
