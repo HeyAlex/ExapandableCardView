fe15fd49e3a8d0f733b68c50f63b14e6eedd5617
a.fialko <a.fialko@corp.mail.ru>
2019-02-25 00:08:04 +0300

    add expand/collapse animation

--- expandable_cardview/src/main/java/com/github/heyalex/expandable_cardview/ExpandableCardView.kt
+++ expandable_cardview/src/main/java/com/github/heyalex/expandable_cardview/ExpandableCardView.kt
@@ -1,5 +1,7 @@
 package com.github.heyalex.expandable_cardview
 
+import android.animation.Animator
+import android.animation.ValueAnimator
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
@@ -7,6 +9,7 @@ import android.view.View
 import androidx.annotation.LayoutRes
 import androidx.cardview.widget.CardView
 import kotlinx.android.synthetic.main.expandable_cardview.view.*
+
 
 class ExpandableCardView @JvmOverloads constructor(
     context: Context,
@@ -22,6 +25,9 @@ class ExpandableCardView @JvmOverloads constructor(
 
     @LayoutRes
     private var contentViewRes: Int = 0
+
+    var isExpanded = true
+        private set
 
     init {
         LayoutInflater.from(context).inflate(R.layout.expandable_cardview, this)
@@ -40,6 +46,90 @@ class ExpandableCardView @JvmOverloads constructor(
 
         card_content.layoutResource = contentViewRes
         contentView = card_content.inflate()
+
+        headerView?.setOnClickListener {
+            if (isExpanded) {
+                isExpanded = false
+                collapse(contentView!!)
+            } else {
+                isExpanded = true
+                expand(contentView!!)
+            }
+
+        }
     }
+
+    private var mAnimator: ValueAnimator? = null
+
+    private fun slideAnimator(start: Int, end: Int): ValueAnimator {
+        val mAnimator = ValueAnimator.ofInt(start, end)
+
+        mAnimator.addUpdateListener { valueAnimator ->
+            //Update Height
+            val value = valueAnimator.animatedValue as Int
+            val layoutParams = contentView?.getLayoutParams()
+            layoutParams?.height = value
+            contentView?.setLayoutParams(layoutParams)
+        }
+        return mAnimator
+    }
+
+    private fun expand(v: View) {
+        v.visibility = View.VISIBLE
+
+        v.measure(
+            View.MeasureSpec.makeMeasureSpec(root.getWidth(), View.MeasureSpec.EXACTLY),
+            View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+        )
+
+        val targetHeight = v.measuredHeight
+
+        mAnimator = slideAnimator(0, targetHeight)
+        mAnimator?.setDuration(400)
+        mAnimator?.start()
+    }
+
+    private fun collapse(v: View) {
+        val finalHeight = v.height
+
+        mAnimator = slideAnimator(finalHeight, 0)
+
+        mAnimator?.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animator: Animator) {
+
+            }
+
+            override fun onAnimationEnd(animator: Animator) {
+                //Height=0, but it set visibility to GONE
+                contentView?.setVisibility(View.GONE)
+            }
+
+            override fun onAnimationCancel(animator: Animator) {
+
+            }
+
+            override fun onAnimationRepeat(animator: Animator) {
+
+            }
+        })
+        mAnimator?.start()
+    }
+//    private fun animateCardView(to: Float) {
+//        contentView?.animate()?.y(to)?.setInterpolator(AccelerateDecelerateInterpolator())
+//            ?.setDuration(400)
+//    }
+//
+//    fun expand() {
+//        isExpanded = true
+//        contentView?.measuredHeight?.toFloat()?.let {
+//            animateCardView(it)
+//        }
+//    }
+//
+//    fun collapse() {
+//        isExpanded = false
+//        animateCardView(0f)
+//    }
+
 
 }
