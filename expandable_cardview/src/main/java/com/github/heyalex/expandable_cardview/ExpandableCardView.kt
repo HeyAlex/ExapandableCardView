a520f17cf3976a102f2084eb476a5a7f28e0a85d
a.fialko <a.fialko@corp.mail.ru>
2019-02-24 22:46:11 +0300

    add ExpandableCardView

---
+++ expandable_cardview/src/main/java/com/github/heyalex/expandable_cardview/ExpandableCardView.kt
@@ -0,0 +1,13 @@
+package com.github.heyalex.expandable_cardview
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.cardview.widget.CardView
+
+class ExpandableCardView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : CardView(context, attrs, defStyleAttr) {
+
+}
