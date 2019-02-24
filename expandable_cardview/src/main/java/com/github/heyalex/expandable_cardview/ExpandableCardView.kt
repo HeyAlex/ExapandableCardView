1c4b9c65848d1e47ecb727cb0a55b51e50ab1e4f
a.fialko <a.fialko@corp.mail.ru>
2019-02-24 22:53:37 +0300

    add inflating of view and get res from attr

--- expandable_cardview/src/main/java/com/github/heyalex/expandable_cardview/ExpandableCardView.kt
+++ expandable_cardview/src/main/java/com/github/heyalex/expandable_cardview/ExpandableCardView.kt
@@ -2,6 +2,9 @@ package com.github.heyalex.expandable_cardview
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import androidx.annotation.IntegerRes
 import androidx.cardview.widget.CardView
 
 class ExpandableCardView @JvmOverloads constructor(
@@ -9,5 +12,22 @@ class ExpandableCardView @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : CardView(context, attrs, defStyleAttr) {
+
+    private var headerView: View? = null
+    private var contentView: View? = null
+
+    @IntegerRes
+    private var headerViewRes: Int = 0
+
+    @IntegerRes
+    private var contentViewRes: Int = 0
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.expandable_cardview, this)
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ExpandableCardView)
+        headerViewRes = typedArray.getResourceId(R.styleable.ExpandableCardView_header_view, View.NO_ID)
+        contentViewRes = typedArray.getResourceId(R.styleable.ExpandableCardView_content_view, View.NO_ID)
+        typedArray.recycle()
+    }
 
 }
